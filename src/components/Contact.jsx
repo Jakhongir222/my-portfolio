111d51ddda1e8e34d090c0835bbf59af4c9258f1
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2023-03-30 01:04:09 +0200

    Need to update css in contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from 'react';
+import email from '../images/email.png';
+import linkedin from '../images/linkedin.png';
+import github from '../images/github.png';
+import '../styles/Contact.css';
 
 export const Contact = () => {
   return (
-    <div>Contact Me</div>
-  )
-}
+    <div className="contact-container">
+      <h1 className="contact-header">Contact Me</h1>
+      <div className="contact-cards">
+        <a href='mailto:jakhongir.burkhanov@appliedtechnology.se' className="contact-card">
+          <img src={email} alt='Email icon' className="contact-icon"/>
+          <h3 className="contact-title">Email</h3>
+          <p className="contact-text">Let's get in touch</p>
+        </a>
+        <a href='https://www.linkedin.com/in/jakburkhanov/' className="contact-card">
+          <img src={linkedin} alt='LinkedIn icon' className="contact-icon"/>
+          <h3 className="contact-title">LinkedIn</h3>
+          <p className="contact-text">Let's connect</p>
+        </a>
+        <a href='https://github.com/Jakhongir222' className="contact-card">
+          <img src={github} alt='GitHub icon' className="contact-icon"/>
+          <h3 className="contact-title">GitHub</h3>
+          <p className="contact-text">Checkout my repos</p>
+        </a>
+      </div>
+    </div>
+  );
+};
