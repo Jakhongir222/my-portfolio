ddeecc80ffa4a66a07231cbe4fc0aa1381cd0d15
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2023-03-30 00:19:30 +0200

    Skills are done

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import React from 'react';
+import skills from './skillsData';
+import frontend from '../images/frontend.webp'
+import backend from '../images/backend.webp'
+import tools from '../images/tools.png'
+import '../styles/Skills.css';
 
 export const Skills = () => {
   return (
-    <div>
-        <h1>My Skills</h1>
-        <p>FrontEnd</p>
-        <p>BackEnd</p>
-        <p>Tools</p>
+    <div className="container">
+      <h1 className="title">My Skills</h1>
+      <div className="column">
+        <img src={frontend} alt='frontend' className="section-icon"/>
+        <h3>FrontEnd</h3>
+        <hr className="line" />
+        <div>
+          {skills.find((skill) => skill.title === 'FrontEnd').skills.map((skill, index) => (
+            <p key={index}>{skill}</p>
+          ))}
+        </div>
+      </div>
+      <div className="column">
+        <img src={backend} alt='backend' className="section-icon"/>
+        <h3>BackEnd</h3>
+        <hr className="line" />
+        <div>
+          {skills.find((skill) => skill.title === 'BackEnd').skills.map((skill, index) => (
+            <p key={index}>{skill}</p>
+          ))}
+        </div>
+      </div>
+      <div className="column">
+        <img src={tools} alt='tools' className="section-icon"/>
+        <h3>Tools</h3>
+        <hr className="line" />
+        <div>
+          {skills.find((skill) => skill.title === 'Tools').skills.map((skill, index) => (
+            <p key={index}>{skill}</p>
+          ))}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
---
+++ src/components/skillsData.js
@@ -0,0 +1,52 @@
+const skills = [
+    {
+      icon: "text-blue-accent-700 text-4xl fab fa-html5",
+      title: "FrontEnd",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React",
+        "Next",
+        "Typescript",
+        "Angular",
+        "TailwindCSS",
+        "OAuth2",
+        "APIs",
+      ],
+    },
+    {
+      icon: "text-blue-accent-700 text-4xl fa-solid fa-code",
+      title: "BackEnd",
+      skills: [
+        "Java",
+        "Maven",
+        "Spring Boot",
+        "MySQL",
+        "MongoDB",
+        "Hibernate",
+        "JPA",
+        "JUnit",
+        "Spring Security",
+        "RESTful APIs",
+      ],
+    },
+    {
+      icon: "text-blue-accent-700 text-4xl fa-solid fa-screwdriver-wrench",
+      title: "Tools",
+      skills: [
+        "Git, Guthub",
+        "Docker",
+        "Vercel",
+        "Microsoft Azure",
+        "VSCode",
+        "Postman",
+        "IntelliJ",
+        "E2E",
+        "Mocha",
+        "Figma",
+      ],
+    }
+  ];
+  
+  export default skills;
