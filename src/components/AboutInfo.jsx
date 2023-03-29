735b0155d67a00caed96c8cdcf8f6a5f3b31b478
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2023-03-29 19:43:21 +0200

    About page is ready

--- src/components/AboutInfo.jsx
+++ src/components/AboutInfo.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React from 'react';
+import info from '../images/info.png';
+import '../styles/AboutInfo.css';
 
 export const AboutInfo = () => {
   return (
-    <div>
-        <h1>About Me</h1>
-        <p>Hey there! I'm a full-stack Java developer with a passion for writing clean, efficient code. 
-            I'm a graduate of the super competitive bootcamp program at School of Applied Technology in Stockholm, 
-            where I learned how to turn my love for OOP and TDD into a career.I'm not just a coder though, 
-            I've got mad problem-solving skills and a keen eye for detail. Plus, my background in Tourism Management has taught me 
-            how to navigate different cultures and communicate effectively with all types of people.
-            So, if you're looking for a driven, adaptable, and all-around awesome full-stack developer, give me a call!</p>
+    <div className="about-info-container">
+      <h1 className="about-title">About Me</h1>
+      <div className="about-content">
+        <img src={info} alt="about page" className="about-image" height={100}/>
+        <div className="about-text">
+        <p>Hey there, I'm Jakhongir Burkhonov, a full-stack Java developer who is passionate about writing clean, 
+         efficient code. I completed the highly competitive bootcamp program at the School of Applied Technology in Stockholm,
+         where I learned to turn my love for object-oriented programming and test-driven development into a successful career.
+        But I'm more than just a coder - I'm also a creative problem-solver with a sharp eye for detail. My background in 
+        Tourism Management has given me experience in communicating effectively with people 
+        from all walks of life.</p>
+        <p>When it comes to technology, I'm comfortable working with a variety of tools and languages, including front-end frameworks, 
+        as well as back-end technologies. Whether I'm building RESTful APIs or designing beautiful, responsive user interfaces,
+        I take pride in delivering work that meets the highest standards of quality and performance. If you're looking for a highly skilled,
+        adaptable, and all-around awesome full-stack developer to join your team, I'm your guy. Check out my skills and projects below.</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
+
