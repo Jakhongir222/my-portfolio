d3cb319c36b7ec24405e644aa173953a0237dede
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2023-03-30 14:25:03 +0200

    Need to work on styling

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,78 @@
 import React from 'react'
+import shoes from '../images/shoes.png';
+import puppy from '../images/puppy.png';
+import workout from '../images/workout.png';
+import travel from '../images/travel.png';
+import fly from '../images/fly.png';
+import tales from '../images/tales.png';
 
 export const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='project-container'>
+      <h1>Projects</h1>
+        <div className='project-card'>
+            <img src={shoes} alt="shoes" height={200} />
+            <h1>Step up Shoes</h1>
+            <h3>E-commerce site to purchase shoes</h3>
+            <p>E-Commerce App, a full stack application that allows users to browse and purchase shoes for Men's, Women's, 
+              and Children's categories. It is built using Java and PostgresSQL in the backend and in the frontend Next13. 
+              It is deployed on Ralways for backend and Vercel for frontend.</p>
+            <button>Live demo</button>
+            <button>Source code</button>
+        </div>
+        <div className='project-card'>
+            <img src={puppy} alt="puppy page" height={200} />
+            <h1>Jak's puppy rescue</h1>
+            <h3>Puppies API where you can do all crud operations</h3>
+            <p>I created an API that allows you to display the initial puppies that exist in the DB on first load.
+              It has the possibility to add a new puppy, update an existing puppy and able to delete a puppy from the DB.
+              I used Java with PostgresSQL in the backend and React Typescript in the frontend. It is deployed in Railways and Vercel
+            </p>
+            <button>Live demo</button>
+            <button>Source code</button>
+        </div>
+        <div className='project-card'>
+            <img src={workout} alt="workout page" height={200} />
+            <h1>Shape Up</h1>
+            <h3>Personal fitness app</h3>
+            <p>I created ShapeUp because I kept forgetting which part to workout when I arrive to the gym. I always wondered 
+              was it Leg day or arm day..? It happened especially when I missed a workout day. So I created this app to keep track 
+              of my workouts. The app was developed using React, Node.js v19, and was deployed on Vercel.</p>
+            <button>Live demo</button>
+            <button>Source code</button>
+        </div>
+        <div className='project-card'>
+            <img src={travel} alt="travel page" height={200} />
+            <h1>PLanet B</h1>
+            <h3>Travel blog</h3>
+            <p>Full - stack appliaction that is build using Java, PostgresSQL and Next.js. It fetches the countries from backend
+              and displays them in frontend. The UI has an intereactive button that shows you which country you will be visiting next.
+              The backend is deployed on Railway and the frontend is deployed on Vercel
+            </p>
+            <button>Live demo</button>
+            <button>Source code</button>
+        </div>
+        <div className='project-card'>
+            <img src={fly} alt="fly page" height={200} />
+            <h1>Fly with me</h1>
+            <h3>Flight finder between Amsterdam, Stockholm and Oslo</h3>
+            <p>Fly with me app is a full-stack application built using React that interacts with a flight finder backend application. 
+              It only shows the flights between Stockholm, Amsterdam and Oslo. It fitlers the flights by the location, and the span that was chosen
+              The database that was used includes only the 3 countries above but if it is expanded, it will work with them as well.
+              </p>
+            <button>Live demo</button>
+            <button>Source code</button>
+        </div>
+        <div className='project-card'>
+            <img src={tales} alt="tales page" height={200} />
+            <h1>Tiny Tales</h1>
+            <h3>Categorized blog with different stories</h3>
+            <p>Tiny Tales is a blog  that is categorized by its categories. It displays the posts as a list of cards. With a clear paragraph body
+               and title for each card. It also includes the tags at the bottom of the card. It has a dropdown for each section. So you can 
+               show and hide the posts for each section. It is developed in React with TypeScript and deployed on Vercel</p>
+            <button>Live demo</button>
+            <button>Source code</button>
+        </div>
+    </div>
   )
 }
