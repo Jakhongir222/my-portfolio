67504097c8750c8e3639a6a6e53f8f1c372dae39
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2023-03-30 21:35:34 +0200

    Ready to deploy

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { About } from './components/About';
 import { AboutInfo } from './components/AboutInfo';
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
+import GoToTheTop from './components/GoToTheTop';
 import { Navbar } from './components/Navbar';
 import { Projects } from './components/Projects';
 import { Skills } from './components/Skills';
@@ -17,6 +18,7 @@ function App() {
       <Projects/>
       <Contact/>
       <Footer/>
+      <GoToTheTop/>
     </div>
   );
 }
