6c20701149a97120f060df72dc83e3c75c6bef31
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2023-03-30 12:24:51 +0200

    V

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import '../styles/Footer.css';
+ 
 
 export const Footer = () => {
-  return (
-    <div>© Copyright 2023 Jakhongir Burkhonov. All rights reserved.</div>
-  )
+    return (
+        <div className="bg-gray-900">
+          <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+            <div className="flex flex-col justify-center pt-5 pb-10 border-t border-gray-800 sm:flex-row">
+              <p className="text-sm text-gray-500">
+                © Copyright 2023 Jakhongir Burkhonov. All rights reserved.
+              </p>
+            </div>
+          </div>
+        </div>
+      );
 }
